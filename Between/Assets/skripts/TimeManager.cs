a90ca05f1c703228ce1ae1de3ff15add296c0433
Cloudly7908 <stangline23632@gmail.com>
2021-11-19 13:06:03 -0500

    SlowMow

--- Between/Assets/skripts/TimeManager.cs
+++ Between/Assets/skripts/TimeManager.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
- 
+    public float Speed = 0.10f;
+    public GameObject watch;
     void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.F))
         {
-            Time.timeScale = 0.10f;
+            Time.timeScale = Speed;
+            watch.SetActive(true);
         }
-        else
+        else 
+        {
             Time.timeScale = 1;
+            watch.SetActive(false);
+        }
+
     }
 }
