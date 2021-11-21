86f4acd2d586f001fb7e48b56ab6ea4d8b1262b7
Black-Chungus <jdaubreyjr20@gmail.com>
2021-11-20 22:51:44 -0600

    FIXED Timer

--- Between/Assets/skripts/Timer.cs
+++ Between/Assets/skripts/Timer.cs
@@ -11,10 +11,21 @@ public class Timer : MonoBehaviour
     float seconds;
     float minutes;
     float timeLeft = 15f;
-    
+    float Millisecond1;
 
+    //int timeTime;
+    int num1;
+    private int timernum()
+    {
+        num1++;
+        if (num1 >= 60)
+        {
+            num1 -= 60;
+        }
+        return num1;
+    }
 
-    [SerializeField] Text timerText;
+    [SerializeField] public Text timerText;
 
     private void Start()
     {
@@ -25,15 +36,33 @@ public class Timer : MonoBehaviour
     {
         timer += Time.deltaTime;
         Millisecond = Mathf.FloorToInt(timeLeft += Time.deltaTime * 100);
-        Debug.Log(Millisecond);
-       if (Millisecond > 60 )
-       {
-            Millisecond = 0.00f;
-       }
+        Millisecond1 =
+
+
+        
+       
         seconds = (int)(timer % 60);
         minutes = (int)((timer / 60) % 60);
    
-        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + Millisecond.ToString("00");
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + timernum().ToString("00");
     }
+    IEnumerator timer1()
+    {
+
+        int time = 0;
+        while(true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            time++;
+            if(time == 60)
+            {
+                time -= 60;
+            }
+            timernum();
+        }
+        
+
+
+    }    
 
 }
